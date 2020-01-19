6214bc96eb916222475a120997161375dad370a8
DinisKhalikov <haodi590@gmail.com>
2020-01-19 15:40:24 +0500

    first

---
+++ src/components/Comments.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <input type="text">
+        <button>Add</button>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Comments"
+    }
+</script>
+
+<style scoped>
+
+</style>
--- src/store/modules/messages.js
+++ src/store/modules/messages.js
@@ -5,7 +5,7 @@ const state = {
 }
 
 const getters = {
-  messages: state => {
+  messages: function(state) {
     return state.messages
   }
 }
