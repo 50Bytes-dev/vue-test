4e8b88db5fe9054b00307512d4cd7402aafd4465
Кучеров Максим <boystoys74@gmail.com>
2020-01-19 14:17:27 +0500

    first

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from django.db import models
 from rest_framework import serializers
 
@@ -5,9 +7,9 @@ from rest_framework import serializers
 class Message(models.Model):
     subject = models.CharField(max_length=200)
     body = models.TextField()
-
+    data = models.DateTimeField(default=datetime.now)
 
 class MessageSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Message
-        fields = ('url', 'subject', 'body', 'pk')
+        fields = ('url', 'subject', 'body', 'data', 'pk')
