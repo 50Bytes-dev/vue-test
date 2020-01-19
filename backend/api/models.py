1741833b6c75dbfaae22732ec7f25a29fa5774d8
Кучеров Максим <boystoys74@gmail.com>
2020-01-19 14:22:44 +0500

    fix date

--- backend/api/models.py
+++ backend/api/models.py
@@ -7,7 +7,7 @@ from rest_framework import serializers
 class Message(models.Model):
     subject = models.CharField(max_length=200)
     body = models.TextField()
-    data = models.DateTimeField(default=datetime.now)
+    date = models.DateTimeField(default=datetime.now)
 
 class MessageSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
